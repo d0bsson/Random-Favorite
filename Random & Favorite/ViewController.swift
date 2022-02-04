89f8f340ea22efd8ded9464eb04c0d553c58c1aa
David <ber.dev@yandex.ru>
2022-02-04 20:28:40 +0300

    Initial Commit

---
+++ Random & Favorite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random & Favorite
+//
+//  Created by Дэвид Бердников on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
